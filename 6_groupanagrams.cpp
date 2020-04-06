f2f9f9cf6ac9e23b44b838448560a781c5f339ff
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-04-06 21:05:58 +0530

    wrote a vague approach because i am in a hurry, gtg

--- 6_groupanagrams.cpp
+++ 6_groupanagrams.cpp
@@ -1,3 +1,15 @@
+/*
+	APPROACH:- first i have created a duplicate of original string type vector, then i have sorted 
+	each string contained in that duplicate string in lexographical order, then i have created a map
+	in which key is gonna be the string contained in that sorted duplicate vector, and its each value
+	is gonna be a vector contains which contains index,then i am simply iterating the map which have 
+	the index as values, and then simply using those indexs in each key,we can simply create a vector
+	and in that vector we can store the group of anagram together and we have to return a vector
+	that contains multiple vectors inside it, thus we have to create multiple vector for each group
+	and push those groups inside the parent vector, i know its a vague explination but, i am running 
+	out of time bros so i hope you are gonna get a little bit after watching the code!  
+*/
+
 vector<vector<string>> groupAnagrams(vector<string>& a) {
     unordered_map<string,vector<int> >m;
     vector<string>b;
