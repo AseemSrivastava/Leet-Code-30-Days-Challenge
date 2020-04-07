2bfede7a2783fdd7bc18190770157f31ba0e66fe
Aseem Srivastava <aseemsrivastava9@gmail.com>
2020-04-07 19:25:17 +0530

    Update 7_countingelements.cpp

--- 7_countingelements.cpp
+++ 7_countingelements.cpp
@@ -14,7 +14,8 @@ Time Complexity:- O(1) because we will always traverse one time
 int countElements(vector<int>& a) {
     int b[1001]={0};
     int counting_elements=0;
-    for(int i=0;i<a.size();++i)++b[a[i]];
+    int n = a.size();
+    for(int i=0;i<n;++i)++b[a[i]];
     for(int i=1;i<=1000;++i)
         if(b[i-1]>0 && b[i]>0)
             counting_elements+=b[i-1];
