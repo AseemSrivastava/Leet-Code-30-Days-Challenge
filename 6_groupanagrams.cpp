0cb8e625495c1a559f8fc00ba0b4843cd916d7f3
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-04-07 12:30:52 +0530

    optimized it by removing duplicate vector

--- 6_groupanagrams.cpp
+++ 6_groupanagrams.cpp
@@ -1,6 +1,6 @@
 /*
-	APPROACH:- first i have created a duplicate of original string type vector, then i have sorted 
-	each string contained in that duplicate string in lexographical order, then i have created a map
+	APPROACH:- first i have sorted 
+	each string and stored it in a temporary string in lexographical order, then i have created a map
 	in which key is gonna be the string contained in that sorted duplicate vector, and its each value
 	is gonna be a vector contains which contains index,then i am simply iterating the map which have 
 	the index as values, and then simply using those indexs in each key,we can simply create a vector
@@ -9,31 +9,24 @@
 	and push those groups inside the parent vector, i know its a vague explination but, i am running 
 	out of time bros so i hope you are gonna get a little bit after watching the code!  
 */
-
-vector<vector<string>> groupAnagrams(vector<string>& a) {
-    unordered_map<string,vector<int> >m;
-    vector<string>b;
-    vector<vector<string>>ret;
-    b=a;
-    for(int i=0;i<b.size();++i)
-    {
-        string s=b[i];
-        sort(s.begin(),s.end());
-        b[i]=s;
-    }
-    for(int i=0;i<b.size();++i)
-    {
-        m[b[i]].emplace_back(i);
-    }
-    for(auto i=m.begin();i!=m.end();++i)
-    {
-        vector<int>v=i->second;
-        vector<string>rets;
-        for(int j=0;j<v.size();++j)
+    vector<vector<string>> groupAnagrams(vector<string>& a) {
+        unordered_map<string,vector<int> >m;
+        vector<vector<string>>ret;
+        for(int i=0;i<a.size();++i)
         {
-            rets.emplace_back(a[v[j]]);
+            string s=a[i];
+            sort(s.begin(),s.end());
+            m[s].emplace_back(i);
         }
-        ret.emplace_back(rets);
+        for(auto i=m.begin();i!=m.end();++i)
+        {
+            vector<int>v=i->second;
+            vector<string>rets;
+            for(int j=0;j<v.size();++j)
+            {
+                rets.emplace_back(a[v[j]]);
+            }
+            ret.emplace_back(rets);
+        }
+        return ret;
     }
-    return ret;
-}
