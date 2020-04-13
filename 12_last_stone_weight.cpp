9482c6ae8426d37156cf2fffcb619b70f18af425
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-04-13 12:41:04 +0530

    Update 12_last_stone_weight.cpp

--- 12_last_stone_weight.cpp
+++ 12_last_stone_weight.cpp
@@ -1,3 +1,36 @@
+// efficient:- using heap
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& a) {
+        make_heap(a.begin(),a.end());
+        while(a.size()!=0)
+        {
+            int t1=a.front();
+            pop_heap(a.begin(),a.end());
+            a.pop_back();
+            if(a.size()!=0)
+            {
+                int t2=a.front();
+                pop_heap(a.begin(),a.end());
+                a.pop_back();
+                if(t1==0)return 0;
+                else if(t1>0 && t2==0)return t1;
+                else a.push_back(t1-t2);
+                make_heap(a.begin(),a.end());
+            }else return t1;
+        }
+        return 0;
+    }
+};
+
+static auto fast=[](){
+    ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    return 0;
+}();
+
+
+// inefficient 
 class Solution {
 public:
     int lastStoneWeight(vector<int>& a) {
@@ -21,5 +54,3 @@ static auto fast=[](){
         cin.tie(NULL);
     return 0;
 }();
-
-// This isn't the optimal one
